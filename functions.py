1cfd5c82a5868f4e2f1989232e177fc8822f9a38
yonioren <why@shiz.shiz>
2025-03-21 10:01:07 +0200

    * main: ** Import all functions from functions file ** Change call to add income function to move user interactions into the function
    * functions:
    ** Add supporting interactive functions
    ** implement add income function

--- functions.py
+++ functions.py
@@ -1,8 +1,50 @@
-def add_income(budget_data, amount, description):
+### Interactive functions ###
+def get_valid_float(prompt):
+    while True:
+        try:
+            if prompt:
+                prompt=prompt.strip()+": "
+            value = float(input(prompt))
+
+            if value > 0:
+                return value
+            else:
+                print("Please enter a value greater than 0.\n")
+        except ValueError:
+            print("Invalid input, please enter a valid number.\n")
+
+def get_valid_string(prompt):
+    while True:
+        value = input(prompt).strip()
+
+        if value:
+            return value
+        else:
+            print("Invalid input, the string cannot be empty.\n")
+
+def get_valid_y_n(prompt):
+    value = get_valid_string(prompt)
+
+    while True:
+        if value.lower() not in ['n','y']:
+            value = get_valid_string("Y/n? ")
+        else:
+            break
+
+    return value.lower() == 'y'
+
+
+def add_income(budget_data):
     """מוסיף הכנסה לתקציב ומעדכן את היתרה"""
-    budget_data["transactions"].append({"type": "income", "amount": amount, "description": description})
-    budget_data["balance"] += amount
-    return budget_data  # מחזיר את הנתונים המעודכנים
+    while True:
+        amount = get_valid_float("Enter income amount: ")
+        description = get_valid_string("Enter description: ")
+
+        budget_data["transactions"].append({"type": "income", "amount": amount, "description": description})
+        budget_data["balance"] += amount
+
+        if not get_valid_y_n("Do you wish to insert another income? "):
+            return budget_data
 
 
 def add_expense(budget_data, amount, description):
